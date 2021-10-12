910fe9607614afd158a78a557b947e5a11d35373
pseudotsuga-fir <hepworta@oregonstate.edu>
2021-10-12 15:29:01 -0700

    complete cploration 3

---
+++ cploration/c03/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX_LINE_LENGTH  200
+
+int main(int argc, const char *argv[]) {
+    if(argc != 2){
+	printf("Usage: %s [filename]\n", argv[0]);
+	exit(EXIT_FAILURE);
+    }
+
+    FILE *fin = fopen( argv[1], "r" );
+    
+    if(!fin) {
+	perror("Unable to open file!");
+	exit(EXIT_FAILURE);
+    }
+
+    char out_file[100];
+    strcpy(out_file, argv[1]);
+    strcat(out_file, ".echo");
+
+    FILE *fout = fopen( out_file, "w+" );
+
+    char line[MAX_LINE_LENGTH] = {0};
+    unsigned int line_num = 0;
+
+    while(fgets(line, sizeof(line), fin)){
+	line_num += 1;
+	printf("[%04d] %s", line_num, line);
+	fprintf(fout, "%s", line);
+    }
+
+    fclose(fin);
+    fclose(fout);
+
+    return 0;
+}
