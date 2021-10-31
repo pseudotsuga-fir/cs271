69605d6fd1e5b47aab5d28ca8b1a57dd03e4b484
pseudotsuga-fir <hepworta@oregonstate.edu>
2021-10-31 15:03:26 -0700

    implement multiplication assembly program

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,27 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// Andrew Hepworth
+  @R2
+  M=0
+  @R1	//Go to first num
+  D=M	//Set data to first num
+  @INFINITE_LOOP
+  D;JEQ
+  @R0
+  D=M
+  @INFINITE_LOOP
+  D;JEQ
+(ADD_R2)
+  @R0
+  D=M
+  @R2
+  M=D+M	//Add first num to itself
+  @R1
+  D=M-1
+  M=D
+  @ADD_R2
+  D;JGT
+(INFINITE_LOOP)
+  @INFINITE_LOOP
+  0;JMP
