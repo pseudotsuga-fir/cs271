f6cfccb13e89aaad62f42135b217fd8c04a4a87c
pseudotsuga-fir <hepworta@oregonstate.edu>
2021-11-02 10:20:49 -0700

    implement Fill assembly program

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,51 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Andrew Hepworth
+(CYCLE)
+  @SCREEN	// Go to the first address of the screen
+  D=A	
+  @address	// Create an address label that holds our current location
+  M=D
+  @8192
+  D=A
+  @counter	// Create a counter label we can decrement and check if it reaches the end of screen
+  M=D
+(LOOP)
+  @KBD
+  D=M
+  @black
+  D;JGT
+  @address	// Go to our current address
+  D=M		// Set D to be out current address
+  A=M		// Jump to the current address
+  M=0		// Set the 16 pixels of this address to white
+  @1
+  D=D+A         // Add one to our current position
+  @address
+  M=D           // Update our address
+  @counter
+  M=M-1
+  D=M
+  @LOOP
+  D;JGT
+(black)
+  @address      // Go to our current address
+  D=M           // Set D to be out current address
+  A=M           // Jump to the current address
+  M=-1          // Set the 16 pixels of this address to black
+  @1
+  D=D+A		// Add one to our current position
+  @address	
+  M=D		// Update our address
+  @counter
+  M=M-1
+  D=M
+  @LOOP
+  D;JGT
+  @CYCLE
+  0;JMP
+(INFINITE_LOOP)
+  @INFINITE_LOOP
+  0;JMP
+
